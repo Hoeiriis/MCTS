56602591d68c46f79cd84ea0d39c634d444b99a0
Alex <alexnlarsen@gmail.com>
2019-09-17 12:21:51 +0200

    updated imports so they work outside of pycharm

--- MCTS_Prototyping/main.py
+++ MCTS_Prototyping/main.py
@@ -1,9 +1,9 @@
-from MCTS_Prototyping.MCTS import MCTS
-from MCTS_Prototyping.MCTS.tree_policies import *
-from MCTS_Prototyping.MCTS.best_child import *
-from MCTS_Prototyping.MCTS.default_policies import *
-from MCTS_Prototyping.MCTS.backpropagation import *
-from MCTS_Prototyping.Environments.tictactoe import Board
+from MCTS import MCTS
+from MCTS.tree_policies import *
+from MCTS.best_child import *
+from MCTS.default_policies import *
+from MCTS.backpropagation import *
+from Environments.tictactoe import Board
 
 if __name__ == "__main__":
 
