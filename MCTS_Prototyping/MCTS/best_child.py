b14c245da9f8c2dbd436c38773a8f0fd828dd58e
Jeppe H. M <jmort15@student.aau.dk>
2019-09-17 13:47:20 +0200

    Fixed another crucial bug

--- MCTS_Prototyping/MCTS/best_child.py
+++ MCTS_Prototyping/MCTS/best_child.py
@@ -5,7 +5,7 @@ def UCT_best_child(node, current_player, c=1 / np.sqrt(2)):
     child_scores = []
     child_visits = []
     for child in node.children:
-        child_scores.append(node.score[current_player])
+        child_scores.append(child.score[current_player])
         child_visits.append(child.visits)
 
     best_child = np.argmax(np.divide(child_scores, np.asarray(child_visits)+0.000000001) + \
