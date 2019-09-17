04ccc15930b3ca1ea1d42b72a5168396e5107de8
Jeppe H. M <jmort15@student.aau.dk>
2019-09-17 12:13:52 +0200

    Reversed a "bug" that actually was not a bug anyway

--- MCTS_Prototyping/MCTS/backpropagation.py
+++ MCTS_Prototyping/MCTS/backpropagation.py
@@ -14,12 +14,8 @@ def backup_negamax(node, default_policy_score, current_player):
         node.visits += 1
         node.score[current_player] += default_policy_score
 
-        other_player_score = -1*default_policy_score
-        other_player = 0 if current_player == 1 else 1
-        node.score[other_player] += other_player_score
-
-        current_player = other_player
-        default_policy_score = other_player_score
+        default_policy_score = -1*default_policy_score
+        current_player = 0 if current_player == 1 else 1
 
 
 def get_ancestors(node):
