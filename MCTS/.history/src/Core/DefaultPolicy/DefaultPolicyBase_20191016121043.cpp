83c699c9864287057a9f60e85a2195ab0bd4284a
beba1 <adrianamijacika@gmail.com>
2019-10-16 14:19:17 +0200

    History files removed

--- MCTS/.history/src/Core/DefaultPolicy/DefaultPolicyBase_20191016121043.cpp
+++
@@ -1,6 +0,0 @@
-#include <DefaultPolicyBase.h>
-
-DefaultPolicyBase::DefaultPolicyBase(function<States(State&)>& getValidChildStates,
-                                     function<Reward(State&)>& evaluateTerminalState):
-                                     getValidChildStates(getValidChildStates),
-                                     evaluateTerminalState(evaluateTerminalState){}
--- MCTS/.history/src/Core/DefaultPolicy/DefaultPolicyBase_20191016123121.cpp
+++
@@ -1,6 +0,0 @@
-#include <DefaultPolicyBase.h>
-
-DefaultPolicyBase::DefaultPolicyBase(boost::function<States(State&)>& getValidChildStates,
-                                     boost::function<Reward(State&)>& evaluateTerminalState):
-                                     getValidChildStates(getValidChildStates),
-                                     evaluateTerminalState(evaluateTerminalState){};
