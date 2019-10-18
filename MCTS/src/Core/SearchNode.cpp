0bfbad69314810d1bd975901b028d9895798bbdd
Jeppe H. Mortensen <jeppe.h.m@hotmail.com>
2019-10-18 11:11:04 +0200

    Implemented SearchNode - Might be missing some functionality, but could not remember any more for now

--- MCTS/src/Core/SearchNode.cpp
+++ MCTS/src/Core/SearchNode.cpp
@@ -1,5 +1,25 @@
 //
 // Created by happysun on 09/10/2019.
 //
-
 #include "include/SearchNode.h"
+
+SearchNode::SearchNode(SearchNode *parent_node, State<boost::any> &state)
+:parent(parent_node),
+state(state),
+visits(0),
+score(0)
+{
+    if(parent_node)
+    {
+        parent->add_child(*this);
+    }
+};
+
+void SearchNode::add_child(SearchNode &child_node)
+{
+    child_nodes.push_back(child_node);
+}
+void SearchNode::set_unvisited_child_states(std::vector<State<boost::any>> &possible_child_states)
+{
+    unvisited_child_states = possible_child_states;
+}
