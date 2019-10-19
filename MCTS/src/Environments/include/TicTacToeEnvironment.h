8bcd6a0a5054e11143066574c40cde4d3609ac90
OpieOP <alexnlarsen@gmail.com>
2019-10-19 17:48:38 +0200

    Moved TicTacToe winconditions into header so they can be used in tests. Renamed ifndef define

--- MCTS/src/Environments/include/TicTacToeEnvironment.h
+++ MCTS/src/Environments/include/TicTacToeEnvironment.h
@@ -12,10 +12,14 @@
  *
  */
 
+#ifndef TICTACTOEENVIRONMENT_H
+#define TICTACTOEENVIRONMENT_H
 
-#ifndef TICTACENVIRONMENT_H
-#define TICTACENVIRONMENT_H
 #include <EnvironmentBase.h>
+
+static const std::vector<std::vector<int>> TicTacToeWinConditions = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, // rows
+                                                                     {0, 3, 6}, {1, 4, 6}, {2, 5, 8}, // columns
+                                                                     {0, 4, 8}, {2, 4, 6}};           // diagonals
 
 enum BoardPiece { None = 0, Cross = 1, Circle = 2 };
 typedef std::vector<BoardPiece> BoardState;
