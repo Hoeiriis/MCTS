b0f3ae38dbfa73ce0629a244692a8683a10d619f
Jeppe H. M <jmort15@student.aau.dk>
2019-10-30 14:16:36 +0100

    forgot this one

--- src/Core/tree_policies/UCT_TreePolicy.cpp
+++ src/Core/tree_policies/UCT_TreePolicy.cpp
@@ -2,7 +2,7 @@
 
 SearchNode UCT_TreePolicy::treePolicy(SearchNode &node) {
 
-    SearchNode current_node = node;
+    SearchNode &current_node = node;
     while (current_node.isTerminalState) {
 
         if (current_node.unvisited_child_states.size() != 0) {
