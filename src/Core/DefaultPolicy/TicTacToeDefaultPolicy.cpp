8a278a3deddb5560bba7d9c01c3a4e94f632fc15
beba1 <adrianamijacika@gmail.com>
2019-10-30 14:53:52 +0100

    TicTacToe Default Policy modified: changed local variable from a vector of valid states to the pointer to the vector

--- src/Core/DefaultPolicy/TicTacToeDefaultPolicy.cpp
+++ src/Core/DefaultPolicy/TicTacToeDefaultPolicy.cpp
@@ -5,28 +5,21 @@ TicTacToeDefaultPolicy::TicTacToeDefaultPolicy(boost::function<States(State<boos
     : DefaultPolicyBase(getValidChildStates, evaluateTerminalState){};
 
 Reward TicTacToeDefaultPolicy::defaultPolicy(State<boost::any> state) {
-    States childStates = this->getValidChildStates(state);
-    States validChildStates;
-    // TODO check the init of tempstate
-    State<boost::any> tempState(NULL);
-
+    States *validChildStates = &(this->getValidChildStates(state));
     std::srand((int)time(0));
 
-    while (childStates.size()) {
-
+    while (validChildStates->size() != 0) {
         // Checking if any of the valid states are terminal (have no valid child states)
-        for (int i = 0; i < childStates.size(); i++) {
-            tempState = childStates[i];
-            validChildStates = this->getValidChildStates(tempState);
-            if (validChildStates.size() == 0) {
-                return this->evaluateTerminalState(tempState);
+        // TODO: add some other condition for checking terminal state except having no valid chlid states
+        for (auto *childState : validChildStates) {
+            if ((this->getValidChildStates(*childState)).size() == 0) {
+                return (this->evaluateTerminalState(*childState));
             }
         }
         // If there is no terminal child choose a state randomly
-        int i;
-        i = std::rand() % childStates.size();
-        tempState = childStates[i];
-        childStates = this->getValidChildStates(tempState);
+        int i = std::rand() % validChildStates->size();
+        state = validChildStates[i];
+        validChildStates = &(this->getValidChildStates(state));
     }
 
     return (this->evaluateTerminalState(state));
