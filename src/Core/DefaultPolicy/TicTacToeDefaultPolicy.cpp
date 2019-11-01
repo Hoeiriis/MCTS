72dff16f1dad5d0c852b566ab260a49282af616d
beba1 <adrianamijacika@gmail.com>
2019-11-01 09:26:42 +0100

    Switched back to regular vector for validChildStates

--- src/Core/DefaultPolicy/TicTacToeDefaultPolicy.cpp
+++ src/Core/DefaultPolicy/TicTacToeDefaultPolicy.cpp
@@ -5,21 +5,21 @@ TicTacToeDefaultPolicy::TicTacToeDefaultPolicy(boost::function<States(State<boos
     : DefaultPolicyBase(getValidChildStates, evaluateTerminalState){};
 
 Reward TicTacToeDefaultPolicy::defaultPolicy(State<boost::any> state) {
-    States *validChildStates = &(this->getValidChildStates(state));
+    //States *validChildStates = &(this->getValidChildStates(state));
+    States validChildStates = this->getValidChildStates(state);
     std::srand((int)time(0));
 
-    while (validChildStates->size() != 0) {
+    while (validChildStates.size() != 0) {
         // Checking if any of the valid states are terminal (have no valid child states)
-        // TODO: add some other condition for checking terminal state except having no valid chlid states
-        for (auto *childState : validChildStates) {
-            if ((this->getValidChildStates(*childState)).size() == 0) {
-                return (this->evaluateTerminalState(*childState));
+        for (auto childState : validChildStates) {
+            if ((this->getValidChildStates(childState)).size() == 0) {
+                return (this->evaluateTerminalState(childState));
             }
         }
         // If there is no terminal child choose a state randomly
-        int i = std::rand() % validChildStates->size();
+        int i = std::rand() % validChildStates.size();
         state = validChildStates[i];
-        validChildStates = &(this->getValidChildStates(state));
+        validChildStates = this->getValidChildStates(state);
     }
 
     return (this->evaluateTerminalState(state));
