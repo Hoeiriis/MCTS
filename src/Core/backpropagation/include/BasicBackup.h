01394ff4befd56247b135685946bee434ac5201c
Jeppe H. M <jmort15@student.aau.dk>
2019-11-21 11:26:10 +0100

    Removed backprop, changed RandomSamplingDefaultPolicy to proper random implementation

--- src/Core/backpropagation/include/BasicBackup.h
+++
@@ -1,8 +0,0 @@
-#include <BackupBase.h>
-
-class BasicBackup : BackupBase {
-  public:
-    BasicBackup();
-    ~BasicBackup();
-    virtual void backup(SearchNode &node, Reward defaultPolicyScore) override;
-};
